53eced9cc6777043b8992223d394875a4dfeb871
NwophokeIsrael <nwophoke@gmail.com>
2023-11-07 16:56:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+// import rootReducer from './Redux/Reducers/reducer';
+// import AddTask from './Components/Addtask';
+// import TaskList from './Components/ListTask';
 import './App.css';
+import rootReducer from './Reducers/reducers';
+import AddTask from './Component/AddTask';
+import ListTask  from './Component/ListTask'
+import Task from './Component/Task';
 
-function App() {
+const store = createStore(rootReducer);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="app-container">
+        <div>
+            <h1 className="app-title">TO DO APPLICATION</h1>
+            <div className="task-section">
+              <AddTask />
+            </div>
+            <div className="task-section">
+              <h2 className="section-title-one">All Tasks</h2>
+              <ListTask />
+            </div>
+            <div className="task-section">
+              <h2 className="section-title-three">Pending Tasks</h2>
+              <ListTask filter="notDone" />
+            </div>
+            <div className="task-section">
+              <h2 className="section-title-two">Completed Tasks</h2>
+              <ListTask filter="done" />
+            </div>
+        </div>
+      </div>
+    </Provider>
   );
-}
+};
 
 export default App;
